ed9093fc995db620a271f40664e09d1d767f5267
Peter Abolude <aboludepeter@gmail.com>
2022-08-10 22:59:46 +0100

    10th August

--- index.mjs
+++ index.mjs
@@ -6,7 +6,6 @@ const startingBalance = stdlib.parseCurrency(100);
 
 const [accAlice, accBob] = await stdlib.newTestAccounts(2, startingBalance);
 console.log("Hello, Alice and Bob!");
-
 console.log("Launching...");
 const ctcAlice = accAlice.contract(backend);
 const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
@@ -45,102 +44,46 @@ const randomCards = () => {
   return blackJackGame["cards"][randomIndex];
 };
 
-let AliceCardsValue = [];
-let BobCardsValue = [];
-let AliceScore = 0;
-let BobScore = 0;
-let totalScore = [];
-let cardValue = {
-  value: "",
-};
-let whoScore = {
-  score: 0,
-};
-const OUTCOME = ["Bob wins", "Draw", "Alice wins"];
+let cards = [];
+
 const Player = (who) => ({
   ...stdlib.hasRandom,
-  PlayerCard: () => {
-    let card = randomCards();
-    if (card == "A") {
-      if (whoScore.score + blackJackGame["cardsMap"][card][1] > 10) {
-        cardValue.value = blackJackGame["cardsMap"][card][0];
-      } else {
-        cardValue.value = blackJackGame["cardsMap"][card][1];
-      }
-    } else {
-      cardValue.value = blackJackGame["cardsMap"][card];
+  dealCards: () => {
+    for (let i = 0; i < 2; i++) {
+      let card = randomCards();
+      cards.push(blackJackGame["cardsMap"][card]);
     }
-
-    if (who == "Alice") {
-      AliceCardsValue.push(cardValue.value);
-      AliceScore = AliceCardsValue.reduce((a, b) => {
-        return a + b;
-      }, 0);
-    } else {
-      BobCardsValue.push(cardValue.value);
-      BobScore = BobCardsValue.reduce((a, b) => {
-        return a + b;
-      }, 0);
-    }
-    console.log(`${who} played ${card}`);
-    return cardValue;
-  },
-
-  seeCardValue: (card) => {
-    console.log(
-      `${who} card value is ${
-        card ? cardValue.value : blackJackGame["cardsMap"][card]
-      }`
-    );
-    return cardValue.value;
-  },
-
-  totalCardValue: () => {
-    AliceScore = AliceCardsValue.reduce((a, b) => {
-      return a + b;
-    }, 0);
-    BobScore = BobCardsValue.reduce((a, b) => {
-      return a + b;
-    }, 0);
-    totalScore = [];
-    totalScore.push(AliceScore, BobScore);
-    return totalScore;
+    console.log(`${cards[0]} and ${cards[1]} were drawn`);
+    return [[cards[0] + cards[1]], String(cards[0])];
   },
   seeOutcome: (outcome) => {
-    console.log(`${who} saw outcome ${OUTCOME[outcome]} `);
+    console.log(`${who} saw ${outcome}`);
+  },
+  viewOpponentCards: (card) => {
+    console.log(`${who} played ${card}`);
   },
 });
 
 console.log("Starting backends...");
 await Promise.all([
   backend.Alice(ctcAlice, {
+    ...stdlib.hasRandom,
     ...Player("Alice"),
+    // implement Alice's interact object here
     wager: stdlib.parseCurrency(5),
-    aliceScore: () => {
-      console.log(`Alice Score is ${AliceScore}`);
-      return AliceScore;
+    revealCards: () => {
+      let cardValue = cards.toString();
+      return cardValue.replaceAll(",", "");
     },
   }),
   backend.Bob(ctcBob, {
+    ...stdlib.hasRandom,
     ...Player("Bob"),
+    // implement Bob's interact object here
     acceptWager: (amt) => {
       console.log(`Bob accepts the wager of ${fmt(amt)}.`);
-      extraCard: () => {
-        return Math.floor(Math.random() * 12);
-      };
-    },
-    bobScore: () => {
-      console.log(`Bob score is ${BobScore}`);
-      return BobScore;
     },
   }),
 ]);
-console.log(AliceCardsValue);
-console.log(BobCardsValue);
-console.log(`{total alice card is ${totalScore[0]}}`);
-console.log(`{total bob card is ${totalScore[1]}}`);
+
 console.log("Goodbye, Alice and Bob!");
-// To - DO
-// Push card in array..
-// Implement logic for A card
-// implement logic to play when less than 16 or >= 21
